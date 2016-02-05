d9403743730f75207ccac34e2905e1a4adee375c
Alexander Litvinenko <litvinenko1706@gmail.com>
2016-02-05 15:38:12 +0200

    15-auth . + authorization

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 import jinja2
 import datetime
 import os
 import logging
 
+from google.appengine.api import users
+
 jinja = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class HomePage(webapp2.RequestHandler):
+class _BaseHandler(webapp2.RequestHandler):
+    """docstring for _BaseHandler"""
+    def initialize(self, request, response):
+        super(_BaseHandler, self).initialize(request,response)
+        self.user = users.get_current_user()
+        if self.user:
+            self.context = {
+                'user': self.user,
+                'is_admin': users.is_current_user_admin(),
+                'logout_url': users.create_logout_url('/'),
+                'versionid': os.environ['CURRENT_VERSION_ID']}
+        else:
+            self.context = {'login_url': users.create_login_url(self.request.url)}
+
+
+class HomePage(_BaseHandler):
     def get(self):
         logging.info('Home page requested')
         template = jinja.get_template('home.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(self.context))
 
 app = webapp2.WSGIApplication([
+    ('/achievements', HomePage),
+    ('/cheevers', HomePage),
+    ('/profile', HomePage),
+    ('/calendar', HomePage),
+    ('/admin', HomePage),
     ('/', HomePage),
-    ('/achievements', HomePage)
 ], debug=True)
